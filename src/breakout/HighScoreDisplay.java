32f42a782b03a068d9a46b33217c1d52049a4006
Jason Qiu <jason.qiu@duke.edu>
2020-10-03 01:03:59 -0400

    add java doc to high score classes

--- src/breakout/HighScoreDisplay.java
+++ src/breakout/HighScoreDisplay.java
@@ -5,12 +5,20 @@ public class HighScoreDisplay extends NumericDisplay {
   private int highScore;
   private final HighScoreReader scoreReader;
 
+  /**
+   * Creates a display showing the high score
+   * @param highScore int representing a high score to be displayed
+   */
   public HighScoreDisplay(int highScore) {
     super("High Score", highScore, GameStatus.HIGH_SCORE_DISPLAY_XPOS, GameStatus.DISPLAY_YPOS);
     this.highScore = highScore;
     scoreReader = new HighScoreReader();
   }
 
+  /**
+   * Updates high score if the current score is greater than the high score
+   * @param currentScore int representing the current score
+   */
   public void updateHighScore(int currentScore) {
     if (currentScore >= highScore) {
       highScore = currentScore;
@@ -19,23 +27,35 @@ public class HighScoreDisplay extends NumericDisplay {
     }
   }
 
+  /**
+   * Updates display with high score
+   */
   @Override
   public void updateDisplay() {
     setText("High Score: " + highScore);
   }
 
+  /**
+   * Resets display with original high score
+   */
   @Override
   public void resetDisplayValue() {
     highScore = scoreReader.readInHighScore();
   }
 
+  /**
+   * Clears high score and replaces it with 0
+   */
   public void clearHighScore() {
     highScore = 0;
     scoreReader.replaceHighScore(highScore);
     updateDisplay();
   }
 
-  // for testing
+  /**
+   * Gets the current high score
+   * @return int representing the current high score
+   */
   public int getHighScore() {
     return highScore;
   }
--- src/breakout/HighScoreReader.java
+++ src/breakout/HighScoreReader.java
@@ -8,6 +8,10 @@ public class HighScoreReader {
 
   private static String filePath = "data/highscore/highscores.txt";
 
+  /**
+   * Returns the high score stored in the file storing high scores
+   * @return int that represents the high score
+   */
   public int readInHighScore() {
     try {
       Scanner scanner = new Scanner(new File(filePath));
@@ -17,6 +21,10 @@ public class HighScoreReader {
     }
   }
 
+  /**
+   * Writes new high score to file
+   * @param updatedHighScore int representing a new updated high score to write to file
+   */
   public void replaceHighScore(int updatedHighScore) {
     try {
       FileWriter fileWriter = new FileWriter(new File(filePath), false);
@@ -27,7 +35,10 @@ public class HighScoreReader {
     }
   }
 
-  // testing purposes
+  /**
+   * Testing: changes the filepath of the high score directory
+   * @param filePath String representing the file path of the high score text file
+   */
   public static void setHighScoreFilePath(String filePath) {
     HighScoreReader.filePath = filePath;
   }
